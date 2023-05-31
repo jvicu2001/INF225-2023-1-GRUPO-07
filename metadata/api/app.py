274ec64b8f6357f1975ce9174f1cc8195475df3a
Jorge Vicuña <jvicuna2001@gmail.com>
2023-05-30 21:48:05 -0400

    Esqueleto microservicio metadata

---
+++ metadata/api/app.py
@@ -0,0 +1,36 @@
+import os
+from fastapi import Body, FastAPI, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, HTMLResponse
+from bson import ObjectId
+
+from typing import List
+
+from models import GeoTiffMetadataModel, PyObjectId
+
+
+
+import motor.motor_asyncio
+
+app = FastAPI()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.Metadata
+
+@app.post("/", response_model=GeoTiffMetadataModel, response_model_exclude_unset=True, response_description="Add metadata from GeoTiff file")
+async def add_metadata(metadata: GeoTiffMetadataModel = Body(...)):
+    metadata = jsonable_encoder(metadata)
+    new_metadata = await db["GeoTiffMetadata"].insert_one(metadata)
+    created_metadata = await db["GeoTiffMetadata"].find_one({"_id": new_metadata.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_metadata)
+
+@app.get("/", response_description="Get list of metadata from GeoTiff files", response_model=List[GeoTiffMetadataModel])
+async def get_metadata(page: int = 0, limit: int = 25):
+    metadata = await db["GeoTiffMetadata"].find().skip(page).limit(limit).to_list(limit)
+    return metadata
+
+@app.get("/{id}", response_description="Get metadata from GeoTiff file", response_model=GeoTiffMetadataModel)
+async def get_metadata_by_id(id: str):
+    if (metadata := await db["GeoTiffMetadata"].find_one({"_id": id})) is not None:
+        return metadata
+
+    raise HTTPException(status_code=404, detail=f"Metadata {id} not found")
