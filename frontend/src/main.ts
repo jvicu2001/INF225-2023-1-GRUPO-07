c90d7d49be722280f784151b898a8d5beac85a52
Jorge Vicuña <jvicuna2001@gmail.com>
2023-07-04 01:34:34 -0400

    Recreado esqueleto en Svelte + Pico.css

--- frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createVuetify } from 'vuetify'
-
-const vuetify = createVuetify()
-
-createApp(App).use(vuetify).mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true })
-  ],
-  server: {
-    hmr: {host: '0.0.0.0'}
-  }
-})
+	plugins: [sveltekit()]
+});
