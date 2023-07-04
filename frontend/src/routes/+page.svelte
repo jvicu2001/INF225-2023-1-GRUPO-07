195f7f55ab43f64413b410ce9c675db9d4e67289
dinosaurio5000 <102392700+dinosaurio5000@users.noreply.github.com>
2023-07-04 22:50:21 +0000

    landing  y edicion login

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,61 @@
 <script lang="ts">
+    import mapa from '$lib/images/mapa-chile.png'
+    import ilustra from '$lib/images/ilu.png'
 </script>
 
-<div class="container">
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- displays site properly based on user's device -->
+  <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+  <title>Datos altimetricos de Chile</title>
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+
+  
+  <header class="hero">
+    <div class="hero__wave"></div>
+
+    <section class="hero__main container">
+        <img src={mapa}/>
+
+      <h1 class="hero__title">Aqui encontraras los dato altimetrico de Chile que necesites.</h1>
+      <p class="hero__paragraph"> Nuestra plataforma te permite buscar y descargar datos altimetricos en formato  GeoTIFF.</p>
+        <a href="#" class="hero__cta">  
+         
+        </a>
+    </section>
+
+  </header>
+
+    <section class="productive container">
+
+      <figure class="productive__picture">
+        <img src={ilustra}/>
+      </figure>
+
+      <div class="productive__texts">
+
+        <h3 class="productive__title"> Nuestros datos siempre estan actualizados</h3>
+
+        <p class="productive__paragraph"> Los administradores actualizan los datos almimetricos cada vez que se obtienen nuevos archivos GeoTIFF.</p>
+
+        <p class="productive__paragraph">Pero recuerda no todas las zonas de nuestro pais cuentan con datos altimetricos.</p>
+
+
+      </div>
+    </section>
+
+</body>
+</html>
+<style>
+    img {
+    max-width: 30%;
+    align-items:end;
+    margin-left:30%;
+    /* height: auto; */
+    /* border-style: none; */
+}
+</style>
--- frontend/src/routes/map/Map.svelte
+++ frontend/src/routes/map/Map.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-   import 'leaflet/dist/leaflet.css';
-   import { Map, TileLayer, Marker, Popup } from 'svelte-map-leaflet';
-
-    const mapOptions = {center:[-35.79348852145885, -71.78329391202294], zoom:4}
-    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-</script>
-
-<div class="container">
-    <h1>Mapa</h1>
-    <div class="map">
-        <Map options={mapOptions}>
-            <TileLayer url={tileUrl}></TileLayer>
-        </Map>
-    </div>
-</div>
-
-<style>
-    .map{
-        height: 500px;
-        width: 300px;
-    }
-</style>
+    import 'leaflet/dist/leaflet.css';
+    import { Map, TileLayer, Marker, Popup } from 'svelte-map-leaflet';
+ 
+     const mapOptions = {center:[-35.79348852145885, -71.78329391202294], zoom:4}
+     const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+ </script>
+ 
+ <div class="container">
+     <h1>Mapa</h1>
+     <div class="map">
+         <Map options={mapOptions}>
+             <TileLayer url={tileUrl}></TileLayer>
+         </Map>
+     </div>
+ </div>
+ 
+ <style>
+     .map{
+ 
+         height: 600px;
+         width: 450px;
+     }
+ </style>
